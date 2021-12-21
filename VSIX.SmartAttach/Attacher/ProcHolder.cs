75ce6af46fb70229fd0cd2d60e7a921d1780b5a5
AminShokrzade1987117 <amin.shokrzade@geeks.ltd.uk>
2021-12-21 18:36:08 +0000

    latest modifications for VS 2022

--- VSIX.SmartAttach/Attacher/ProcHolder.cs
+++ VSIX.SmartAttach/Attacher/ProcHolder.cs
@@ -10,165 +10,223 @@ using GeeksAddin;
 
 namespace Geeks.VSIX.SmartAttach.Attacher
 {
-    extern alias MSharp;
+	extern alias MSharp;
 
-    public class ProcHolder
-    {
-        public EnvDTE80.Process2 Process { get; private set; }
-        public string AppPool { get; private set; }
-        public DateTime? StartTime { get; private set; } = null;
+	public class ProcHolder
+	{
+		public EnvDTE80.Process2 Process { get; private set; }
+		public string AppPool { get; private set; }
+		public DateTime? StartTime { get; private set; } = null;
 
-        static ExcludedProcessesManager excludedProcessesManager = new ExcludedProcessesManager();
-        public static ExcludedProcessesManager ExcludedProcessesManager => excludedProcessesManager;
+		public string ProcessPath { get; private set; }
 
-        static ConcurrentDictionary<int, ProcHolder> alreadyCheckedProcesses = new ConcurrentDictionary<int, ProcHolder>();
+		static ExcludedProcessesManager excludedProcessesManager = new ExcludedProcessesManager();
+		public static ExcludedProcessesManager ExcludedProcessesManager => excludedProcessesManager;
 
-        ProcHolder(EnvDTE80.Process2 prc)
-        {
-            try
-            {
-                Process = prc;
+		static ConcurrentDictionary<int, ProcHolder> alreadyCheckedProcesses = new ConcurrentDictionary<int, ProcHolder>();
 
-                using (var mos = new ManagementObjectSearcher(
-                    @"\\{0}\root\cimv2".FormatWith(prc.TransportQualifier.Replace("Geeks@", "")),
-                    "SELECT CommandLine FROM Win32_Process WHERE ProcessId = " + prc.ProcessID))
-                {
-                    foreach (var mo in mos.Get())
-                    {
-                        var commandLine = mo["CommandLine"];
-                        if (commandLine != null)
-                        {
-                            var cmdLineStr = commandLine.ToString();
+		ProcHolder(EnvDTE80.Process2 prc)
+		{
+			try
+			{
+				Process = prc;
 
-                            if (ExcludedProcessesManager.CheckCommandLine(cmdLineStr.ToLower()))
-                            {
-                                Process = null;
-                                return;
-                            }
+				using (var mos = new ManagementObjectSearcher(
+					@"\\{0}\root\cimv2".FormatWith(prc.TransportQualifier.Replace("Geeks@", "")),
+					"SELECT CommandLine FROM Win32_Process WHERE ProcessId = " + prc.ProcessID))
+				{
+					foreach (var mo in mos.Get())
+					{
+						var commandLine = mo["CommandLine"];
+						if (commandLine != null)
+						{
+							var cmdLineStr = commandLine.ToString();
 
-                            var appPool = GetAppPool(commandLine.ToString());
+							if (ExcludedProcessesManager.CheckCommandLine(cmdLineStr.ToLower()))
+							{
+								Process = null;
+								return;
+							}
 
-                            if (appPool != null) AppPool += appPool;
-                            
-                            var dotNetCoreApp = GetDotNetCoreApp(prc, commandLine.ToString());
+							var appPool = GetAppPool(commandLine.ToString());
 
-                            if (dotNetCoreApp != null) AppPool += dotNetCoreApp;
-                        }
-                    }
-                }
+							if (appPool != null) AppPool += appPool;
 
-                if (AppPool == null) AppPool = Path.GetFileName(Process.Name);
-            }
-            catch (Exception)
-            {
-            }
-        }
-        public static ProcHolder CheckAndAddProcHolder(EnvDTE80.Process2 process)
-        {
-            ProcHolder returnProcessHolder = null;
+							var dotNetCoreApp = GetDotNetCoreApp(prc, commandLine.ToString());
 
-            try
-            {
-                if (process == null) return null;
+							if (dotNetCoreApp != null) AppPool += dotNetCoreApp;
+						}
+					}
+				}
 
-                var startTime = ExcludedProcessesManager.CheckAndReturnStartTime(process);
+				if (AppPool == null) AppPool = Path.GetFileName(Process.Name);
+			}
+			catch (Exception)
+			{
+			}
+		}
 
-                if (startTime == null) return null;
+		public static ProcHolder CheckAndAddProcHolder(EnvDTE80.Process2 process)
+		{
+			ProcHolder returnProcessHolder = null;
 
-                if (alreadyCheckedProcesses.TryGetValue(process.ProcessID, out returnProcessHolder))
-                {
-                    if (returnProcessHolder.Process.Name == process.Name)
-                    {
-                        returnProcessHolder.StartTime = startTime;
-                        return returnProcessHolder;
-                    }
-                }
+			try
+			{
+				if (process == null) return null;
+				if (process.Name.Contains("dotnet.exe")) return null;
+				var startTime = ExcludedProcessesManager.CheckAndReturnStartTime(process);
+				var ProcessPath = process.Name;
 
-                returnProcessHolder = new ProcHolder(process);
-                if (returnProcessHolder.Process != null)
-                {
-                    alreadyCheckedProcesses.TryAdd(process.ProcessID, returnProcessHolder);
-                    returnProcessHolder.StartTime = startTime;
-                    return returnProcessHolder;
-                }
+				if (startTime == null) return null;
 
-                return null;
-            }
-            catch (Exception)
-            {
-            }
+				if (alreadyCheckedProcesses.TryGetValue(process.ProcessID, out returnProcessHolder))
+				{
+					if (returnProcessHolder.Process.Name == process.Name)
+					{
+						returnProcessHolder.StartTime = startTime;
+						returnProcessHolder.ProcessPath = ProcessPath;
+						return returnProcessHolder;
+					}
+				}
 
-            return returnProcessHolder;
-        }
+				returnProcessHolder = new ProcHolder(process);
+				if (returnProcessHolder.Process != null)
+				{
+					alreadyCheckedProcesses.TryAdd(process.ProcessID, returnProcessHolder);
+					returnProcessHolder.StartTime = startTime;
+					returnProcessHolder.ProcessPath = ProcessPath;
+					return returnProcessHolder;
+				}
 
-        public override string ToString()
-        {
-            if (Process == null) return "NULL";
+				return null;
+			}
+			catch (Exception ex)
+			{
+			}
 
-            if (StartTime.HasValue == false)
-                return string.Format("{1} ({0})", Process.ProcessID, AppPool, Process.TransportQualifier).Replace("Geeks@", "");
+			return returnProcessHolder;
+		}
 
-            return AppPool.PadRight(25, ' ') + string.Format(" ({0}: {1})", Process.ProcessID, MSharp::System.MSharpExtensions.ToTimeDifferenceString(StartTime.Value, 1)).Remove("Geeks@");
-        }
+		public override string ToString()
+		{
+			if (Process == null) return "NULL";
 
-        string GetAppPool(string fullCommandLine)
-        {
-            // for IIS
-            var match = Regex.Match(fullCommandLine, @"-ap ""(?<pool>[^""]*)""");
-            if (match.Success)
-                return match.Groups["pool"].Value.Trim();
+			try
+			{
+				if (AppPool.ToLower() != "website.exe")
+					return (AppPool.PadRight(30, ' ') + Process.ProcessID.ToString().PadRight(8, ' ') + Process.Name).Replace("Geeks@", "");
+				else
+				{
+					string ProcName = null;
+					int EndIndex = Process.Name.ToLower().IndexOf(@"\website\bin\");
 
-            var args = Utils.SplitCommandLine(fullCommandLine);
+					if (EndIndex < 0)
+					{
+						return (AppPool.PadRight(30, ' ') + Process.ProcessID.ToString().PadRight(8, ' ') + Process.Name).Replace("Geeks@", "");
+					}
+					int StartIndex = EndIndex - 1;
+					while (StartIndex > 0 && Process.Name[StartIndex - 1] != '\\')
+					{
+						StartIndex--;
+					}
+					ProcName = Process.Name.Substring(StartIndex, EndIndex - StartIndex);
 
-            // for IIS express
-            var siteArg = args.FirstOrDefault(a => a.StartsWith("/site:"));
-            var configArg = args.FirstOrDefault(a => a.StartsWith("/config:"));
+					return ((string.Format("{0} ({1})", ProcName, Path.GetFileNameWithoutExtension(AppPool))).PadRight(30, ' ') + Process.ProcessID.ToString().PadRight(8, ' ') + Process.Name).Replace("Geeks@", "");
+				}
+			}
+			catch
+			{
+				return "NULL";
+			}
 
-            if (siteArg != null && configArg != null)
-            {
-                var site = siteArg.TrimStart("/site:").TrimMatchingQuotes();
-                var configFile = configArg.TrimStart("/config:").TrimMatchingQuotes();
-                try
-                {
-                    // find physical path to this site from configuration file
-                    var xconfig = XDocument.Load(configFile);
-                    var siteNode = xconfig.XPathSelectElement("//site[@name='" + site + "']");
-                    var physicalPath = siteNode.Element("application").Element("virtualDirectory").Attribute("physicalPath").Value;
+			//string Str2 = null;
+			//if (StartTime.HasValue == false)
+			//{
+			//    return (AppPool.PadRight(20, ' ') + Process.ProcessID.ToString().PadRight(28, ' ') + Process.Name).Replace("Geeks@", "");
+			//    //return string.Format("{1} ({0}) ({2}) ({3})", Process.ProcessID, AppPool, Process.TransportQualifier, Process.Name).Replace("Geeks@", "");
+			//}
+			//string Result = AppPool.PadRight(25, ' ');
+			//try
+			//{
+			//    Str2 = string.Format("{0} {1} {2}", Process.ProcessID, MSharp::System.MSharpExtensions.ToTimeDifferenceString(StartTime.Value, 1)).Remove("Geeks@");
+			//}
+			//catch
+			//{
+			//    //Str2= string.Format("{1} ({0}) ({2}) ({3})", Process.ProcessID+ Str2.PadRight(35, ' '), AppPool+ Str2.PadRight(35, ' '), Process.TransportQualifier + Str2.PadRight(35, ' '), Process.Name).Replace("Geeks@", "");
+			//    Str2 = (AppPool.PadRight(20, ' ') + Process.ProcessID.ToString().PadRight(28, ' ') + Process.Name).Replace("Geeks@", "");
+			//    //Str2 = Str2.PadRight(20, ' ');
+			//    //Str2 += Process.ProcessID;
+			//    //Str2 = Str2.PadRight(28, ' ');
+			//    //Str2 += Process.TransportQualifier + "   ";
+			//    //Str2 += Process.Name;
+			//    //Str2 = Str2.Replace("Geeks@", "");
+			//    return Str2;
+			//}
+			//Str2 = Str2.PadRight(35, ' ');
+			//Result += Str2;
+			//Result += Process.Name;
+			//return Result;
+		}
 
-                    return site;
-                }
-                catch
-                {
-                    // just ignore
-                }
-            }
+		string GetAppPool(string fullCommandLine)
+		{
+			// for IIS
+			var match = Regex.Match(fullCommandLine, @"-ap ""(?<pool>[^""]*)""");
+			if (match.Success)
+				return match.Groups["pool"].Value.Trim();
 
-            return null;
-        }
+			var args = Utils.SplitCommandLine(fullCommandLine);
 
-        string GetDotNetCoreApp(EnvDTE80.Process2 prc, string fullCommandLine)
-        {
-            if (fullCommandLine.Contains("dotnet.exe") == false) return null;
-            var match = Regex.Match(fullCommandLine, @"dotnet.exe"".*exec.*""(.*)""");
-            if (match.Success && match.Groups.Count >= 1)
-            {
-                var tp = System.Diagnostics.Process.GetProcessById(prc.ProcessID);
+			// for IIS express
+			var siteArg = args.FirstOrDefault(a => a.StartsWith("/site:"));
+			var configArg = args.FirstOrDefault(a => a.StartsWith("/config:"));
 
-                string title = Path.GetFileName(match.Groups[1].Value.Trim());
+			if (siteArg != null && configArg != null)
+			{
+				var site = siteArg.TrimStart("/site:").TrimMatchingQuotes();
+				var configFile = configArg.TrimStart("/config:").TrimMatchingQuotes();
+				try
+				{
+					// find physical path to this site from configuration file
+					var xconfig = XDocument.Load(configFile);
+					var siteNode = xconfig.XPathSelectElement("//site[@name='" + site + "']");
+					var physicalPath = siteNode.Element("application").Element("virtualDirectory").Attribute("physicalPath").Value;
 
-                try
-                {
-                    var windowtitle = tp.MainWindowTitle;
+					return site;
+				}
+				catch
+				{
+					// just ignore
+				}
+			}
 
-                    if (windowtitle != null) title += " >> " + windowtitle;
-                }
-                catch
-                {
-                }
+			return null;
+		}
 
-                return $"dotnet[\"{title}\"]";
-            }
-            return null;
-        }
-    }
+		string GetDotNetCoreApp(EnvDTE80.Process2 prc, string fullCommandLine)
+		{
+			if (fullCommandLine.Contains("dotnet.exe") == false) return null;
+			var match = Regex.Match(fullCommandLine, @"dotnet.exe"".*exec.*""(.*)""");
+			if (match.Success && match.Groups.Count >= 1)
+			{
+				var tp = System.Diagnostics.Process.GetProcessById(prc.ProcessID);
+
+				string title = Path.GetFileName(match.Groups[1].Value.Trim());
+
+				try
+				{
+					var windowtitle = tp.MainWindowTitle;
+
+					if (!(string.IsNullOrEmpty(windowtitle) || string.IsNullOrWhiteSpace(windowtitle))) title += " >> " + windowtitle;
+				}
+				catch
+				{
+
+				}
+
+				return $"dotnet[\"{title}\"]";
+			}
+			return null;
+		}
+	}
 }
